962ed510a6e4600533bdd69ad2c8a37a0bf1ef43
Admin <Admin@LAPTOP-Q6I9MM59>
2024-02-18 20:21:49 +0300

    Добавьте файлы проекта.

---
+++ OperS_lab2/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace OperS_lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(22, 90);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(290, 260);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(385, 136);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(351, 164);
+            this.listBox2.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ OperS_lab2/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+namespace OperS_lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+            // Получаем информацию о дисках
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.IsReady)
+                {
+                    listBox1.Items.Add("Диск: " + drive.Name);
+                    listBox1.Items.Add("Тип: " + drive.DriveType);
+                    listBox1.Items.Add("Объем: " + drive.TotalSize);
+                    listBox1.Items.Add("Свободно: " + drive.AvailableFreeSpace);
+                    listBox1.Items.Add("");
+                }
+            }
+
+            // Получаем информацию о подключенных устройствах
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive WHERE InterfaceType='USB'");
+            foreach (ManagementObject device in searcher.Get())
+            {
+                string deviceId = device["DeviceID"].ToString();
+                string model = device["Model"].ToString();
+                listBox2.Items.Add("Устройство: " + model + " (" + deviceId + ")");
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Очищаем ListBox перед обновлением данных
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+
+            // Получаем информацию о дисках
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.IsReady)
+                {
+                    listBox1.Items.Add("Диск: " + drive.Name);
+                    listBox1.Items.Add("Тип: " + drive.DriveType);
+                    listBox1.Items.Add("Объем: " + drive.TotalSize);
+                    listBox1.Items.Add("Свободно: " + drive.AvailableFreeSpace);
+                    listBox1.Items.Add("");
+                }
+            }
+            // Получаем информацию о подключенных устройствах
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive WHERE InterfaceType='USB'");
+            foreach (ManagementObject device in searcher.Get())
+            {
+                string deviceId = device["DeviceID"].ToString();
+                string model = device["Model"].ToString();
+                listBox2.Items.Add("Устройство: " + model + " (" + deviceId + ")");
+            }
+        }
+    }
+}
